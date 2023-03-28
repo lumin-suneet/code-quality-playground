fccbf587c2f90bad83759601c3cd97905274a9c2
Suneet Misra <suneetmisra@lumin.co.uk>
2023-03-28 08:36:14 +0100

    style(prettier): adds basic config and formats all files accordingly

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('@commitlint/types').UserConfig} */
 
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  helpUrl:
-    "This repo follows conventional-commit syntax https://www.conventionalcommits.org/en/v1.0.0/ . Please either  format your commit messages to match or run `yarn commit` to access a cli which will step you through constructing a compliant commit message.",
-};
+    extends: ['@commitlint/config-conventional'],
+    helpUrl:
+        'This repo follows conventional-commit syntax https://www.conventionalcommits.org/en/v1.0.0/ . Please either  format your commit messages to match or run `yarn commit` to access a cli which will step you through constructing a compliant commit message.',
+}
