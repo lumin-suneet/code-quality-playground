1ebc53b5a4f5d10ab28835d2cb70ebac009dccb1
Suneet Misra <suneetmisra@lumin.co.uk>
2023-04-04 10:43:47 +0100

    chore(setup): adds eslint-plugin-react-hooks to eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
         'standard-with-typescript',
         'prettier',
     ],
